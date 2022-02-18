ad2fd5a86cbd880d4b56ebc9e4572fb7c5ed940c
JayAvil <98556181+JayAvil@users.noreply.github.com>
2022-02-18 10:00:37 -0500

    2nd

---
+++ Main.py
@@ -0,0 +1,24 @@
+
+from Dino import Dinosaur
+from Robot import Robot
+from Group import Fleet, Herd 
+ 
+class Character: 
+        def __init__(self, name, health, attack_power): 
+            self.name = name
+            self.health = health
+            self.attack_power = attack_power 
+            
+         
+RoboFleet = Fleet([Voltron, Megazord, Optimus])
+DinoHerd = Herd([Tyran, Veloce, Megalo])
+
+
+
+                      
+            
+            
+            
+            
+            
+            
